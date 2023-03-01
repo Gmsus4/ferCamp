0f303659f871a8b8843c41c41b697b498bfde753
fer <fernando452000@gmail.com>
2023-03-01 11:34:01 -0600

    Se agregaron imágenes a los campamentos

--- seeds/index.js
+++ seeds/index.js
@@ -16,9 +16,13 @@ const seedDB = async () =>{
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quisquam ab sapiente, corrupti culpa itaque earum odio mollitia pariatur sint harum aperiam quam ut nihil molestiae. Qui, non laborum! Enim?',
+            price: price
         });
         await camp.save();
     }
