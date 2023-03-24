3219563b242ade09a9d44b90605160e9a2336caf
Fernando <jesusfgm45@gmail.com>
2023-03-24 12:23:17 -0600

    Se cambió la ruta para el funcionamiento de Mongo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
-mongoose.connect('mongodb://localhost:27017/fer-camp');
+mongoose.connect('mongodb://127.0.0.1:27017/fer-camp');
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const Campground = require('../models/campground');
 const {places, descriptors} = require('./seedHelpers');
-mongoose.connect('mongodb://localhost:27017/fer-camp');
+mongoose.connect('mongodb://127.0.0.1:27017/fer-camp');
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
