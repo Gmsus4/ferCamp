ac00ececf24224d6c7a32da7cde988b538280d89
Fernando <jesusfgm45@gmail.com>
2023-06-04 09:35:11 -0600

    Comentamos unas líneas de código en el campgrounds.js routes y reiniciamos la seeds solo para que muestre un campground automáticamente

--- routes/campground.js
+++ routes/campground.js
@@ -7,17 +7,17 @@ const multer = require('multer');
 const { storage } = require('../cloudinary');
  const upload = multer({ storage });
 
-router.route('/')
+router.route('/') // /campgrounds
     .get(catchAsync(campgrounds.index))
     .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.route('/:id')
+router.route('/:id') // /campgrounds/:id
     .get(catchAsync(campgrounds.showCampground))
     .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync (campgrounds.updateCampground))
     .delete(isLoggedIn, isAuthor, catchAsync (campgrounds.deleteCampground))
 
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync (campgrounds.renderEditForm));
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync (campgrounds.renderEditForm)); // /campgrounds/:id/edit
 
 module.exports = router;
