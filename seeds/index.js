12704369f0a24e7700aeed9d1b616df238efa806
Fernando <jesusfgm45@gmail.com>
2023-06-04 19:06:06 -0600

    Content Security Policy - Dolor de cabeza :c

--- seeds/index.js
+++ seeds/index.js
@@ -14,14 +14,13 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () =>{
     await Campground.deleteMany({});
-    for(let i = 0; i < 1; i++){
+    for(let i = 0; i < 5; i++){
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({ //Por cada uno de los campamentos creados automáticamente.
             author: '647a1f217a0e9757a4b263ae',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quisquam ab sapiente, corrupti culpa itaque earum odio mollitia pariatur sint harum aperiam quam ut nihil molestiae. Qui, non laborum! Enim?',
             price: price,
             geometry: {
@@ -30,11 +29,11 @@ const seedDB = async () =>{
             },
             images: [
                 {
-                    url: 'https://source.unsplash.com/collection/483251',
+                    url: 'https://res.cloudinary.com/dozzu7xhx/image/upload/v1685924169/campgroundFer/m3seppv0kfy9gvd4jsbs.jpg',
                     filename: 'yelcamp'
                 },
                 {
-                    url: 'https://source.unsplash.com/collection/483251',
+                    url: 'https://res.cloudinary.com/dozzu7xhx/image/upload/v1685924430/campgroundFer/i8e8hvgf3l8rcutyecj6.jpg',
                     filename: 'yelcamp'
                 }
                 //549 Campground
