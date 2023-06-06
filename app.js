abeea3f5ee3f869a4895418684604b479dc2b333
Fernando <jesusfgm45@gmail.com>
2023-06-05 21:30:40 -0600

    Base de datos a Atlas

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoStore = require('connect-mongo');
-//const dbUrl = process.env.DB_URL;
-const dbUrl = 'mongodb://127.0.0.1:27017/campgroundFer';
+const dbUrl = process.env.DB_URL;
+//const dbUrl = 'mongodb://127.0.0.1:27017/campgroundFer';
 
 const userRoutes = require('./routes/users')
 const campgroundsRoutes = require('./routes/campground');
